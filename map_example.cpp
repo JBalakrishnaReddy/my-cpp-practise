a6b9e06ba3b7a31dd28b673ea0feedd7cde3aab7
Balakrishna Reddy <17263409+JBalakrishnaReddy@users.noreply.github.com>
2023-05-25 08:11:51 +0530

    map_example.cpp
    modfied a little

--- map_example.cpp
+++ map_example.cpp
@@ -4,6 +4,8 @@
 #include <vector>         // std::vector
 #include <functional>     // std::greater
 #include <map>
+#include <unordered_map>
+
 
 using namespace std;
 
@@ -12,6 +14,18 @@ void print_vector(vector<int> & vec, string msg=""){
     for (const int &s : vec) /*insides follow this */   cout << s << ", ";
     cout << endl;
 }
+
+struct compare {
+template <typename T>
+   
+   bool operator()(const T& a, const T& b) 
+   {
+      if (a.second != b.second) {
+         return a.second < b.second;
+      }
+      return a.first < b.first;
+   }
+};
 
 int main(){
     map<int , int, greater<int>> first;
@@ -23,6 +37,10 @@ int main(){
     for(map<int,int>::iterator it=first.begin(); it != first.end(); it++){
         cout << it->first << " " << it->second << endl;
     }
+    cout << first[10] << endl;
+    for(map<int,int>::iterator it=first.begin(); it != first.end(); it++){
+        cout << it->first << " " << it->second << endl;
+    }
     map<char, int> second;
     second['b'] = 10;
     second['c'] = 5;
@@ -30,7 +48,16 @@ int main(){
     for(map<char,int>::iterator it=second.begin(); it != second.end(); it++){
         cout << it->first << " " << it->second << endl;
     }
-
+    vector<int> nums{1,1,1, 1,2,2,3,3, 4,4,4,4};
+    // map<int, int, compare> lis;
+    map<int, int, greater<int> > lis;
+    for(auto i:nums){
+        lis[i] = lis[i]+1;
+    }
+    // map <int,int> lis2(lis);
+    for(map<int,int>::iterator it=lis.begin(); it != lis.end(); it++){
+        cout << it->first << " " << it->second << endl;
+    }
  
 
     // return ;
